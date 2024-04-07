74345d0db4e3f623ee88e5a8554205d12efc6dce
fjrXTR <fajarslebew31@gmail.com>
2024-04-07 17:08:20 +0000

    X6837: Rebrand to Infinix-X6837
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_X678B.mk
+    $(LOCAL_DIR)/twrp_X6837.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X678B-user \
-    twrp_X678B-userdebug \
-    twrp_X678B-eng
+    twrp_X6837-user \
+    twrp_X6837-userdebug \
+    twrp_X6837-eng
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## Device config for TWRP Infinix NOTE 30 Pro ( Infinix-X678B )
+## Device config for TWRP Infinix HOT 40 Pro ( Infinix-X6837 )
